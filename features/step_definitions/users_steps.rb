2c01fbead0865a02461d69f864256b3945851a4c
Danny Santos <danny@ethicalswitch.com>
2017-02-12 00:08:45 +0000

    User updates and styling

--- features/step_definitions/users_steps.rb
+++ features/step_definitions/users_steps.rb
@@ -3,25 +3,21 @@ Given(/^they are on the registration page$/) do
 end
 
 Given(/^a user has previously registered$/) do
-  @user = User.create!(
-    {
-      email: "test@test.com",
-      password: "password",
-      password_confirmation: "password"
-    }
-  )
+  @user = User.create!({
+    email: "test@test.com",
+    password: "password",
+    password_confirmation: "password"
+  })
 end
 
-Given(/^they are on the log in page$/) do
+Given(/^they are on the sign in page$/) do
   visit new_user_session_path
 end
 
 Given(/^they have signed in previously$/) do
   steps %{
-    Given they are on the log in page
-    When they fill in the "user_email" field with "test@test.com"
-      And they fill in the "user_password" field with "password"
-      And they click on "Log in"
+    Given they are on the sign in page
+    When they fill in the sign in form
   }
 end
 
@@ -29,14 +25,27 @@ Given(/^they are on the home page$/) do
   visit root_path
 end
 
-Then(/^they are logged in$/) do
+When(/^they fill in the sign in form$/) do
+  fill_in :user_email, with: "test@test.com"
+  fill_in :user_password, with: "password"
+  click_on "Log in"
+end
+
+When(/^they fill in the user registration form$/) do
+  fill_in :user_email, with: "test@test.com"
+  fill_in :user_password, with: "password"
+  fill_in :user_password_confirmation, with: "password"
+  click_on "Sign up"
+end
+
+Then(/^they are signed out$/) do
+  expect(page).to have_content("Signed out successfully.")
+end
+
+Then(/^they are signed in$/) do
   expect(page).to have_content("Signed in successfully.")
 end
 
 Then(/^a user should be created$/) do
   expect(User.last.email).to eq('test@test.com')
-end
-
-Then(/^they should see a farewell message$/) do
-  expect(page).to have_content("Signed out successfully.")
 end
--- features/users.feature
+++ features/users.feature
@@ -1,24 +1,19 @@
-Feature: users
+Feature: Users
 
   Scenario: A user registers
     Given they are on the registration page
-    When they fill in the "user_email" field with "test@test.com"
-      And they fill in the "user_password" field with "password"
-      And they fill in the "user_password_confirmation" field with "password"
-      And they click on "Sign up"
+    When they fill in the user registration form
     Then a user should be created
-    
+  
   Scenario: A user signs in
     Given a user has previously registered
-      And they are on the log in page
-    When they fill in the "user_email" field with "test@test.com"
-      And they fill in the "user_password" field with "password"
-      And they click on "Log in"
-    Then they are logged in
-    
+      And they are on the sign in page
+    When they fill in the sign in form
+    Then they are signed in
+  
   Scenario: A user signs out
     Given a user has previously registered
       And they have signed in previously
       And they are on the home page
     When they click on "Sign out"
-    Then they should see a farewell message
+    Then they are signed out
